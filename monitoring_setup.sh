3a02ceaa46f2c981f2aee4827a4814099bff15dd
André Lameirinhas <andre.lameirinhas@gmail.com>
2022-06-17 03:31:12 +0100

    Remove set -e and improve monitoring setup
    Removing set -e is necessary because it allow crontab -l to fail
    in case there's no crontab file for the current user.

--- monitoring_setup.sh
+++ monitoring_setup.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
-set -euo pipefail
+set -uo pipefail
 
 wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-armv7.tar.gz
 tar xf node_exporter-1.3.1.linux-armv7.tar.gz
+rm node_exporter-1.3.1.linux-armv7.tar.gz
 NODE_EXPORTER=$(readlink -f node_exporter-1.3.1.linux-armv7/node_exporter)
-(crontab -l || true; echo "@reboot ${NODE_EXPORTER} &") | crontab -
+(crontab -l 2> /dev/null; echo "@reboot ${NODE_EXPORTER} &") | crontab -
+echo "monitoring setup finished with success"
