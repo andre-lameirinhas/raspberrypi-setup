3d266ccb29993c1463d3edfeb19f06d5200e77e5
André Lameirinhas <andre.lameirinhas@gmail.com>
2022-07-25 01:05:02 +0100

    Improve set_library script

--- set_library.sh
+++ set_library.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 
 
-mkdir /disks /disks/elements
-chown -R plex:plex /disks
+sudo mkdir -p /disks/elements
+sudo chown -R plex:plex /disks
 
 mount_point="
 #Plex Library
 UUID=CC94BC7094BC5EA0 /disks/elements ntfs defaults,auto,rw,nofail 0 1
 "
-printf "$mount_point" >> /etc/fstab
+sudo bash -c "printf '$mount_point' >> /etc/fstab"
 
 umount /media/pi/Elements
 mount /disks/elements
