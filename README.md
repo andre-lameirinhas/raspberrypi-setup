6c7603f668416a3dd6191454a4fad8961ba00ce9
André Lameirinhas <andre.lameirinhas@gmail.com>
2022-10-19 01:33:56 +0100

    Add cron to check for plex upgrades
    Also, improve README and add TODOs and newlines at the end of files.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Scripts to setup Plex Media Server on a Raspberry Pi
+
+## plex_setup.sh
+- download and install plex
+
+## set_library.sh
+- set the plex library on the external hdd
+
+## monitoring_setup.sh
+- setup a node_exporter
--- monitoring_setup.sh
+++ monitoring_setup.sh
@@ -5,5 +5,6 @@ wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_e
 tar xf node_exporter-1.3.1.linux-armv7.tar.gz
 rm node_exporter-1.3.1.linux-armv7.tar.gz
 NODE_EXPORTER=$(readlink -f node_exporter-1.3.1.linux-armv7/node_exporter)
+# TODO remove duplicates
 (crontab -l 2> /dev/null; echo "@reboot ${NODE_EXPORTER} &") | crontab -
 echo "monitoring setup finished with success"
