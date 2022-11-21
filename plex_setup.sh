1b2963c9638982c04ffb72497d3819f7f008fdff
André Lameirinhas <andre.lameirinhas@gmail.com>
2022-11-21 02:17:58 +0000

    Add missing -y and sudo, remove possible crontab duplicates

--- plex_setup.sh
+++ plex_setup.sh
@@ -17,16 +17,15 @@ static domain_name_servers=192.168.1.1 1.1.1.1 8.8.8.8
 printf "$static_ip_config" >> /etc/dhcpcd.conf
 
 # Install plex media server
-sudo apt-get update
-sudo apt install apt-transport-https ca-certificates curl
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 sudo apt update
 sudo apt install plexmediaserver
 echo "plex installation finished with success"
 
-# Setup a daily cron to check for updates and upgrade plex if found
-# TODO remove duplicates
 # TODO add timestamp to logs
-(crontab -l 2> /dev/null; echo "0 5 * * * sudo apt update; sudo apt upgrade plexmediaserver -y >> /var/log/plex_upgrade.log") | crontab -
+# convert crontab command to bash script
+(crontab -l 2> /dev/null; echo "0 5 * * * sudo /bin/bash -c 'apt update; apt --only-upgrade install -y plexmediaserver >> /var/log/plex_upgrade.log'") | awk '!x[$0]++' | crontab -
 echo "daily upgrade check set to 5am, logs on /var/log/plex_upgrade.log"
--- set_library.sh
+++ set_library.sh
@@ -12,4 +12,4 @@ UUID=CC94BC7094BC5EA0 /disks/elements ntfs defaults,auto,rw,nofail 0 1
 sudo bash -c "printf '$mount_point' >> /etc/fstab"
 
 umount /media/pi/Elements
-mount /disks/elements
+sudo mount /disks/elements
